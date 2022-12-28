21120a4b3446ced20b64c9af12f158d4e46491dd
absolutely-no-one <autumn20071@icloud.com>
2022-12-28 18:21:30 -0500

    Save sets

--- new-set.js
+++ new-set.js
@@ -19,3 +19,18 @@ function addTerm() {
 
     terms.appendChild(container);
 }
+
+function createSet() {
+    const element = document.getElementById("terms");
+    var terms = []
+    for (var i = 0; i < element.children.length; i++) {
+        var term = {}
+        term["term"] = element.children[i].children[0].value; // if length < 1, throw error
+        term["definition"] = element.children[i].children[1].value; // if length < 1, throw error
+        terms.push(term);
+    }
+    firebase.database().ref("sets").push().set({
+        "name": document.getElementById("title").value,
+        "terms": terms
+    })
+}
