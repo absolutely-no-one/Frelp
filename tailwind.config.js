366039076410cdb5fbcf670e1aecea91df0a9481
absolutely-no-one <autumn20071@icloud.com>
2023-04-11 14:03:58 -0400

    Added color presets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "french-red": "#ED2939",
+        "french-blue": "#002654"
+      }
+    },
+    screens: {
+      "xsm": "375px"
+    }
   },
   plugins: [],
 }
