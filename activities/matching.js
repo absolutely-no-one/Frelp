4fa3a65a041c29f2d7986faa8678214445cfcc1f
absolutely-no-one <ntaylor0725@icloud.com>
2023-08-03 21:20:17 -0400

    Finish screen added

--- activities/matching.js
+++ activities/matching.js
@@ -8,6 +8,9 @@ var cards = [];
 var cardSelected = false;
 var selectedCardId = "";
 var animating = false;
+var guesses = 0;
+var totalMatches = 0;
+var cardsFlipped = [];
 
 function generateTitle() {
     const data = firebase.database().ref("/sets/" + type + "/" + id);
@@ -38,7 +41,7 @@ function generateBoard() {
     for (var i = 0; i < totalMatches; i++) {
         var card = document.createElement("div");
         card.setAttribute("id", i + 1);
-        card.setAttribute("class", "lg:w-24 lg:h-20 m-2 p-3 bg-french-blue text-white text-center flex flex-col justify-center rounded-lg hover:cursor-pointer");
+        card.setAttribute("class", "w-11/12 h-20 m-2 p-1 bg-french-blue text-white text-xl font-semibold text-center flex flex-col justify-center rounded-lg hover:cursor-pointer");
         card.text = setData[i].term;
         card.addEventListener("click", function() {
             checkCard(this.id, this.text);
@@ -46,11 +49,13 @@ function generateBoard() {
 
         var matchingCard = document.createElement("div");
         matchingCard.setAttribute("id", (i + 1) * -1);
-        matchingCard.setAttribute("class", "lg:w-24 lg:h-20 m-2 p-3 bg-french-blue text-white text-center flex flex-col justify-center rounded-lg hover:cursor-pointer");
+        matchingCard.setAttribute("class", "w-11/12 h-20 m-2 p-1 bg-french-blue text-white text-xl font-semibold text-center flex flex-col justify-center rounded-lg hover:cursor-pointer");
         matchingCard.text = setData[i].definition;
         matchingCard.addEventListener("click", function() {
             checkCard(this.id, this.text);
         })
+
+        cardsFlipped.push([0,0]);
 
         cards.push(card);
         cards.push(matchingCard);
@@ -64,19 +69,31 @@ function generateBoard() {
 }
 
 function checkCard(card, text) {
-    if (animating == false) {
+    if (animating == false && card != selectedCardId) {
         document.getElementById(card).innerHTML = text;
+
+        if (card < 0) {
+            cardsFlipped[(Number(card) + 1) * -1][0] += 1;
+        } else {
+            cardsFlipped[card - 1][1] += 1;
+        }
+
         if (cardSelected == false) {
             selectedCardId = card;
             cardSelected = true;
         } else {
             animating = true;
+            guesses ++;
             if (selectedCardId == card * -1) {
                 setTimeout(() => {
                     document.getElementById(card).remove();
                     document.getElementById(selectedCardId).remove();
                     animating = false;
                     cardSelected = false;
+
+                    if (document.getElementById("cardGrid").children.length == 0) {
+                        finishGame();
+                    }
                 }, 500);
             } else {
                 setTimeout(() => {
@@ -88,4 +105,73 @@ function checkCard(card, text) {
             }
         }
     }
+}
+
+function finishGame() {
+    document.getElementById("cardGrid").style.display = "none";
+    var grid = document.getElementById("game");
+
+    var finishContainer = document.createElement("div");
+    finishContainer.setAttribute("class", "bg-french-blue text-white w-11/12 md:w-5/12 mx-auto my-4 rounded-md p-2")
+
+    var congratsMessage = document.createElement("div");
+    congratsMessage.setAttribute("class", "text-3xl lg:text-4xl px-2 pt-1 text-center font-semibold");
+    var randNum = Math.floor(Math.random() * 5);
+
+    switch (randNum) {
+        case 1:
+            congratsMessage.innerHTML = "Wahoo!";
+        break;
+        case 2:
+            congratsMessage.innerHTML = "The force is strong with this one.";
+        break;
+        case 3:
+            congratsMessage.innerHTML = "Fish & visitors stink after 3 days";
+        break;
+        case 4:
+            congratsMessage.innerHTML = "Epic.";
+        break;
+        default:
+            congratsMessage.innerHTML = "One small step...";
+    }
+
+    var lineDivider = document.createElement("hr");
+    lineDivider.setAttribute("class", "border-amber border-[1px] xl:border-2 rounded-full my-2");
+
+    var stats = document.createElement("div");
+    stats.setAttribute("class", "text-center text-white text-xl lg:text-2xl");
+
+    var highest = 0;
+    for (var i = 0; i < cardsFlipped.length; i++) {
+        if (cardsFlipped[i][0] > highest) {
+            highest = cardsFlipped[i][0];
+        } 
+        if (cardsFlipped[i][1] > highest) {
+            highest = cardsFlipped[i][1];
+        }
+    }
+
+    stats.innerHTML = "It took you " + guesses + " tries to get " + totalMatches + " matches.  The most times you flipped the same card was " + highest + ".";
+
+    var playAgain = document.createElement("div");
+    playAgain.innerHTML = "Play again";
+    playAgain.setAttribute("class", "text-white w-11/12 md:w-1/3 bg-french-blue text-center py-1 rounded-md mx-auto text-xl sm:text-2xl xl:text-3xl hover:cursor-pointer hover:underline decoration-amber");
+    playAgain.addEventListener("click", function() {
+        window.location.reload();
+    });
+
+    var home = document.createElement("div");
+    home.innerHTML = "View other games";
+    home.setAttribute("class", "text-white w-11/12 md:w-1/3 my-2 bg-french-blue text-center py-1 rounded-md mx-auto text-xl sm:text-2xl xl:text-3xl hover:cursor-pointer hover:underline decoration-amber");
+    home.addEventListener("click", function() {
+        goBackFromGame();
+    });
+
+    finishContainer.appendChild(congratsMessage);
+    finishContainer.appendChild(lineDivider);
+    finishContainer.appendChild(stats);
+
+    grid.appendChild(finishContainer);
+    grid.appendChild(playAgain);
+    grid.appendChild(home);
 }
