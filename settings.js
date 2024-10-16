a2a8ed5675ad47cf18d27c8c5905c9efee796e6d
absolutely-no-one <ntaylor0725@icloud.com>
2024-10-15 22:09:21 -0400

    Add reporting for select users

---
+++ settings.js
@@ -0,0 +1,56 @@
+// fix this function 
+function getReports() {
+
+    firebase.auth().onAuthStateChanged((currentUser) => {
+        if (currentUser.displayName != "ntaylor0725" || currentUser.uid != "AyG7zZ6ghYQhx14Bd9RJBXSGXu53") {
+            return;
+        }
+    })
+
+    var reports = firebase.database().ref("/reports");
+    var parent = document.getElementById("reportData");
+
+    reports.once("value").then((snapshot) => {
+        snapshot.forEach((childSnapshot) => {
+            var report = childSnapshot.val();
+            var reportRef = firebase.database().ref("reports/" + childSnapshot.key);
+            var set = firebase.database().ref("sets/" + report.type + "/" + report.id);
+            var setData = "";
+
+            set.once("value").then((setSnapshot) => {
+                setData = setSnapshot.val();
+
+            var container = document.createElement("div");
+            container.setAttribute("id", childSnapshot.key);
+            var reportName = document.createElement("div");
+            reportName.innerHTML = setData.name;
+
+            var reportReason = document.createElement("div");
+            reportReason.innerHTML = report.reason;
+
+            var remove = document.createElement("div");
+            remove.innerHTML = "Remove set";
+            remove.addEventListener("click", () => {
+                reportRef.remove();
+                set.remove();
+                document.getElementById(childSnapshot.key).remove();
+            });
+
+            var keep = document.createElement("div");
+            keep.innerHTML = "Keep set";
+            keep.addEventListener("click", () => {
+                reportRef.remove();
+                document.getElementById(childSnapshot.key).remove();
+            });
+
+        
+            container.appendChild(reportName);
+            container.appendChild(reportReason);
+            container.appendChild(remove);
+            container.appendChild(keep);
+
+            parent.appendChild(container);
+            })
+        })
+    })
+}
